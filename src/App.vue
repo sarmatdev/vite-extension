7238fa50abf848774e5d53ad2355eaf5ede94159
sarmat <skhachirov12@gmail.com>
2021-09-15 19:12:49 +0300

    feat: app styles

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,42 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <hr />
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
+::-webkit-scrollbar {
+  width: 0;
+  background: transparent;
+}
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  width: 100vw;
+  min-width: 300px;
+  height: 100vh;
+  min-height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  overflow: hidden;
+  background-color: #73a3c4;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #617ba3;
+  overflow: scroll;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.5;
+  width: 360px;
+  height: 600px;
+  background-color: #fff;
 }
 </style>
