1b61af88e12a4b43b0441d88c14eeb832a5ec96d
sarmat <skhachirov12@gmail.com>
2021-09-16 20:56:39 +0300

    feat: useWeb3 composable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <select v-model="currentNetwork" name="" id="">
+      <option :value="0">Mainnet</option>
+      <option :value="1">Testnet</option>
+    </select>
+    {{ currentNetwork }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import { useWeb3 } from '@/composables/useWeb3'
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const web3 = useWeb3()
+    const currentNetwork = ref(1)
+
+    watch(currentNetwork, () => {
+      web3.handleNetworkChanged(currentNetwork.value)
+    })
+
+    return {
+      currentNetwork
+    }
+  }
 })
 </script>
