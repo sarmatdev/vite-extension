bf880dc524fc26fc70124cbbbb030bc574d39878
sarmat <skhachirov12@gmail.com>
2021-09-16 20:56:10 +0300

    feat: networks config

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export default {
+  networks: [
+    {
+      id: 1,
+      name: 'Mainnet',
+      httpUrl: 'https://node-tokyo.vite.net/http',
+      wsUrl: 'wss://node-tokyo.vite.net/ws'
+    },
+    {
+      id: 2,
+      name: 'Testnet',
+      httpUrl: 'https://buidl.vite.net/gvite/http',
+      wsUrl: 'wss://buidl.vite.net/gvite/ws'
+    },
+    {
+      id: 3,
+      name: 'Local',
+      httpUrl: 'http://127.0.0.1:9500',
+      wsUrl: ''
+    }
+  ]
+}
