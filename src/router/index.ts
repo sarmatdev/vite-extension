267381379fa31b7ce5463f555a0979854c4fdd49
sarmat <skhachirov12@gmail.com>
2021-09-17 18:20:45 +0300

    feat: basic route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { getStorageItem } from '@/services/storage'
 import Home from '../views/Home.vue'
+
+async function checkAuth(to: any, from: any, next: any) {
+  const isAuth = await getStorageItem('isAuth')
+  console.log(isAuth)
+  if (isAuth) {
+    next()
+  } else {
+    next('/')
+  }
+}
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +21,8 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue'),
+    beforeEnter: checkAuth
   }
 ]
 
