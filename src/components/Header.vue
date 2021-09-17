2f561404998be642b3cbf84561f5e6b6035d5068
sarmat <skhachirov12@gmail.com>
2021-09-17 22:35:10 +0300

    feat: header component

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="h-12 flex items-center justify-between px-2">
+    <img class="h-8" src="@/assets/images/logo-blue1.svg" alt="" />
+    <select v-model="currentNetwork">
+      <option :value="0">Mainnet</option>
+      <option :value="1">Testnet</option>
+    </select>
+    <base-button to="/menu" color="white" circle flat>
+      <base-icon name="menu" size="lg" />
+    </base-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+import { useWeb3 } from '@/composables/useWeb3'
+export default defineComponent({
+  name: 'Header',
+  setup() {
+    const web3 = useWeb3()
+
+    const currentNetwork = ref(0)
+
+    watch(currentNetwork, () => {
+      web3.handleNetworkChanged(currentNetwork.value)
+    })
+
+    return {
+      currentNetwork
+    }
+  }
+})
+</script>
+
+<style></style>
