f5b93544dad2050f19380c1b90669f5f57885521
sarmat <skhachirov12@gmail.com>
2021-09-19 11:05:55 +0300

    fix: layout

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="welcome">
+  <div class="px-2">
+    <h1 class="text-center text-2xl">Welcome to</h1>
     <img
       class="mt-12 mb-6"
-      src="../assets/images/vite-logo.png"
+      src="../assets/images/vite-logo-blue@2x.png"
       width="360"
       alt="Welcome Image"
     />
     <div>
-      <h1 class="text-gray-600 font-black text-lg">Wallet is not connected</h1>
-      <p class="my-20">You can import an existing wallet or create a new one</p>
+      <p class="">You can import an existing wallet or create a new one</p>
     </div>
     <div class="flex justify-around items-center space-x-3">
       <BaseButton size="lg" color="blue" to="/create-wallet">Create</BaseButton>
-      <BaseButton size="lg" color="blue" to="/import-wallet">Import</BaseButton>
+      <BaseButton size="lg" color="blue" outline to="/import-wallet"
+        >Import</BaseButton
+      >
     </div>
   </div>
 </template>
@@ -22,11 +24,3 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({})
 </script>
-
-<style lang="scss">
-.welcome {
-  @apply text-center flex flex-col p-3;
-  &__hero {
-  }
-}
-</style>
