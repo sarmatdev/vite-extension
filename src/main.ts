09fc654de46f4557391357ed4eab630812e6b832
sarmat <skhachirov12@gmail.com>
2021-09-22 15:43:15 +0300

    feat: click outside directive

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import registerBaseComponents from './helpers/registerBaseComponents'
+import registerDirectives from './helpers/registerDirectives'
 import './assets/styles/tailwind.css'
 
 export const app = createApp(App)
 
 registerBaseComponents(app)
+registerDirectives(app)
 
 app.use(store).use(router).mount('#app')
