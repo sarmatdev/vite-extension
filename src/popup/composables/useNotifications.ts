a4199e6bc9fb33ae5c35bb930eb08adb3d1f2a31
sarmat <skhachirov12@gmail.com>
2021-10-13 09:19:36 +0300

    feat: add UI and chrome notifications

---
+++ src/popup/composables/useNotifications.ts
@@ -0,0 +1,35 @@
+import { ref } from 'vue'
+
+export interface chromeNotification {
+  title: string
+  message: string
+}
+
+export interface Notification {
+  message: string
+  type: string
+  timestamp?: number
+}
+
+const items = ref<Notification[]>([])
+
+export function useNotifications() {
+  function notify(payload: Notification) {
+    items.value.push({ ...payload, timestamp: Date.now() })
+    console.log(items.value)
+  }
+
+  function chromeNotify(params: chromeNotification): void {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icons/128.png',
+      ...params
+    })
+  }
+
+  return {
+    items,
+    notify,
+    chromeNotify
+  }
+}
