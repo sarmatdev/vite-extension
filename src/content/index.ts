720309800f855d5a84db5011c73767ebfc77a4d9
sarmat <skhachirov12@gmail.com>
2021-10-19 18:27:24 +0300

    feat: inject wallet provider to the page

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello from content!')
+chrome.runtime.sendMessage({ action: 'GET_TAB_ID_INNER_EVENT_REQUEST' }, () => {
+  injectScript()
+})
+
+function injectScript() {
+  try {
+    const node = document.head || document.documentElement
+    const script = document.createElement('script')
+    script.setAttribute('type', 'text/javascript')
+    script.setAttribute('src', chrome.extension.getURL('inject-script.js'))
+    node.appendChild(script)
+    console.info('✅ ViteWallet provider injected')
+  } catch (e) {
+    console.error('🚨 ViteWallet provider injection failed', e)
+  }
+}
