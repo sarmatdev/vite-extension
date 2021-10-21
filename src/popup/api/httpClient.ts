70d24f16a5b2d34ee4a5f80cf5bd9ec2587b277d
AlanNabiev <alannabiev2001@gmail.com>
2021-10-21 19:23:17 +0300

    feat: tokens & prices api

---
+++ src/popup/api/httpClient.ts
@@ -0,0 +1,29 @@
+import { useWeb3 } from '@/composables/useWeb3'
+import axios from 'axios'
+
+const { state } = useWeb3()
+const baseURL = state.network.httpVitex
+
+const httpClient = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export function get(url: string, data?: any) {
+  return httpClient({
+    method: 'get',
+    url,
+    params: data
+  })
+}
+
+export function post(url: string, data: any) {
+  return httpClient({
+    method: 'post',
+    url,
+    data
+  })
+}
+
