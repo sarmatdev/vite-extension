468fdfa337cae81072a906bed7665a706b2d4a60
sarmat <skhachirov12@gmail.com>
2021-11-12 18:56:06 +0300

    fix: prod script

--- build/webpack/prod.ts
+++ build/webpack/prod.ts
@@ -1,19 +1,7 @@
 import merge from 'webpack-merge'
-import { resolve } from 'path'
 import common from './common'
-import { fromSrc } from './common'
 
 export default merge(common, {
   mode: 'production',
-  devtool: 'hidden-source-map',
-  entry: {
-    background: [fromSrc('/background/index.ts')],
-    popup: fromSrc('/popup/index.ts'),
-    'content-script': fromSrc('/content/index.ts')
-  },
-  output: {
-    path: resolve('./dist'),
-    pathinfo: false,
-    filename: '[name].js'
-  }
+  devtool: 'hidden-source-map'
 })
