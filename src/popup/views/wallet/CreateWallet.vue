dc0f666607c35c0003368a08c5c9bb8c215ffbc5
Алан Набиев <alannabiev@MacBook-Air-Alan.local>
2021-11-17 06:52:14 +0300

    feat: mnemoniic copy/paste, validation

--- src/popup/views/wallet/CreateWallet.vue
+++ src/popup/views/wallet/CreateWallet.vue
@@ -2,25 +2,23 @@
   <div class="p-2">
     <h1 class="my-4">Create wallet</h1>
     <main v-if="scene == 1">
-      <section class="h-full w-full flex flex-col space-y-4">
-        <BaseWarning name="info">
+      <section class="h-full w-full flex flex-col">
+        <BaseWarning class="mt-4" name="info">
           Write down or copy this phrase in the correct order and keep it in a
           safe place.
         </BaseWarning>
 
         <BaseTextarea
           label="Your recovery phrase"
-          class="mt-4"
+          class="mt-8 mb-28"
+          icon="copy"
+          @iconEvent="copyRecoveryPhrase"
           v-model="mnemonic"
           readonly
         />
 
-        <BaseButton color="gradient" rounded size="s" class="mx-auto">
-          <BaseIcon class="mr-0.5" size="s" name="copy" />
-          Copy
-        </BaseButton>
-        <BaseCheckbox v-model="agree">
-          I understand that if i lose my recovery phrase, i will not beable to
+        <BaseCheckbox class="mx-2" v-model="agree">
+          I understand that if i lose my recovery phrase, i will not be able to
           access my funds.
         </BaseCheckbox>
       </section>
@@ -50,27 +48,32 @@
       <main v-if="scene == 2" class="confirm_mnemonic">
         <section class="grid grid-cols-1 gap-y-8 p-3">
           <BaseInput
+            @input="requiedV$.validator.$touch"
             v-model="name"
             placeholder="Name"
+            :errors="requiedError"
             label="Set the wallet name"
           ></BaseInput>
           <BaseTextarea
-            label="You must write the phrase in the correct order"
-            v-model="mnemonicForConfirmString"
-            readonly
+            @input="CMV$.$touch"
+            label="Write or paste the phrase in the correct order"
+            v-model="mnemonicForConfirm"
+            icon="clipboard"
+            :errors="confrimMnemonicErrors"
+            @iconEvent="pasteRecoveryPhrase"
           ></BaseTextarea>
           <ul class="space-x-2 flex flex-wrap justify-center">
             <li
               v-for="(mnemonicItem, idx) in randomMnemonic"
               :key="idx"
               @click="addMnemonic(mnemonicItem)"
-              :disabled="mnemonicForConfirm.includes(mnemonicItem)"
+              :disabled="hasAdded(mnemonicItem)"
               :class="
-                mnemonicForConfirm.includes(mnemonicItem)
+                hasAdded(mnemonicItem)
                   ? 'bg-gray-300 cursor-default'
                   : 'cursor-pointer btn-grad'
               "
-              class="border text-gray-600 rounded-2xl py-1 px-4 mb-2"
+              class="border rounded-2xl py-1 px-4 mb-2"
             >
               {{ mnemonicItem }}
             </li>
@@ -87,7 +90,9 @@
               @click="saveWallet"
               color="blue"
               size="lg"
-              :disabled="!mnemonicConfirmed"
+              :disabled="
+                !mnemonicConfirmed || confrimMnemonicErrors.length || !name
+              "
               >Create Wallet</BaseButton
             >
           </div>
@@ -106,6 +111,8 @@ import {
   validatePrivateKey,
   createFromMnemonic
 } from '../../../services/AccountService'
+import useClipboard from '@/composables/useClipboard'
+import useValidate from '@/composables/useValidate'
 
 export default defineComponent({
   name: 'CreateWallet',
@@ -116,6 +123,23 @@ export default defineComponent({
     let scene = ref(1)
     const wallet = createRandom()
     const mnemonic = wallet.mnemonic
+    const mnemonicForConfirm = ref('')
+    const { writeClipboard, readClipboard } = useClipboard()
+
+    const { CMV$, confrimMnemonicErrors } = useValidate({
+      mnemonic: mnemonic,
+      mnemonicForConfirm: mnemonicForConfirm
+    })
+
+    function copyRecoveryPhrase() {
+      writeClipboard(mnemonic)
+    }
+    async function pasteRecoveryPhrase() {
+      const clipboard = await readClipboard()
+      mnemonicForConfirm.value = clipboard
+      CMV$.value.$touch()
+    }
+
     const randomMnemonic = computed(() => {
       return mnemonic
         .split(' ')
@@ -128,34 +152,35 @@ export default defineComponent({
       scene.value = 2
     }
 
-    const mnemonicForConfirm = ref([])
-    const mnemonicForConfirmString = computed(() => {
-      return mnemonicForConfirm.value.join(' ')
-    })
+    function hasAdded(word: string): boolean {
+      return !!mnemonicForConfirm.value.split(' ').find((el) => el === word)
+    }
 
     function clearMnemonic() {
-      mnemonicForConfirm.value = []
+      mnemonicForConfirm.value = ''
+      CMV$.value.$touch()
     }
 
     function addMnemonic(item: string): void {
-      //@ts-ignore
-      if (!mnemonicForConfirm.value.includes(item)) {
-        //@ts-ignore
-        mnemonicForConfirm.value.push(item)
+      CMV$.value.$touch()
+      if (!mnemonicForConfirm.value.split(' ').includes(item)) {
+        mnemonicForConfirm.value = [
+          ...mnemonicForConfirm.value.split(' '),
+          item
+        ].join(' ')
       }
     }
 
     const mnemonicConfirmed = computed(() => {
-      return validatePrivateKey(mnemonicForConfirmString.value)
+      return validatePrivateKey(mnemonicForConfirm.value)
     })
 
     const fromMnemonic = computed(() => {
       return createFromMnemonic(mnemonic)
     })
-    console.log(mnemonic)
 
     const name = ref(`Wallet ${accountsNum.value + 1}`)
-
+    const { requiedV$, requiedError } = useValidate({ validator: name })
     function saveWallet() {
       store.dispatch('wallets/storeWallet', {
         name: name.value,
@@ -172,6 +197,7 @@ export default defineComponent({
       router,
       mnemonic,
       randomMnemonic,
+      hasAdded,
       continueToConfirm,
       agree,
       wallet,
@@ -179,9 +205,14 @@ export default defineComponent({
       mnemonicForConfirm,
       clearMnemonic,
       addMnemonic,
-      mnemonicForConfirmString,
       mnemonicConfirmed,
-      saveWallet
+      saveWallet,
+      copyRecoveryPhrase,
+      pasteRecoveryPhrase,
+      CMV$,
+      confrimMnemonicErrors,
+      requiedV$,
+      requiedError
     }
   }
 })
@@ -211,36 +242,21 @@ export default defineComponent({
 .btn-grad {
   background-image: linear-gradient(
     to right,
-    #000046 0%,
-    #1cb5e0 51%,
-    #000046 100%
-  );
-}
-
-.btn-grad {
-  background-image: linear-gradient(
-    to right,
-    #1fa2ff 0%,
-    #12d8fa 51%,
-    #1fa2ff 100%
-  );
-}
-
-.btn-grad {
-  background-image: linear-gradient(
-    to right,
-    #1c92d2 0%,
-    #f2fcfe 51%,
-    #1c92d2 100%
+    #457fca 0%,
+    #8dc3f5 51%,
+    #457fca 100%
   );
 }
 .btn-grad {
   transition: 0.5s;
   background-size: 200% auto;
-  box-shadow: 0 0 20px #eee;
+  color: white;
+  box-shadow: 0 0 20px rgb(231, 230, 230);
+  display: block;
 }
 
 .btn-grad:hover {
   background-position: right center; /* change the direction of the change here */
+  color: #fff;
 }
 </style>
