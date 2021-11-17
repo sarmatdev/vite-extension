25427073d7ef4a9e16868d1bff47520a12c096bd
Алан Набиев <alannabiev@MacBook-Air-Alan.local>
2021-11-17 06:53:24 +0300

    feat: select shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
+    boxShadow: {
+      select:
+        '0px 0px 4px rgba(71, 73, 79, 0.1), 0px 1px 2px rgba(71, 73, 79, 0.25);'
+    },
     extend: {}
   },
   variants: {
