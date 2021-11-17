cc6aa64fa62516e869c1ed1cf3cecc23a9851476
Алан Набиев <alannabiev@MacBook-Air-Alan.local>
2021-11-17 06:48:48 +0300

    feat: textarea icon/styles

--- src/popup/components/base/BaseTextarea.vue
+++ src/popup/components/base/BaseTextarea.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="relative">
     <label
       :for="label"
       class="block pl-1 font-medium cursor-pointer text-gray-700"
@@ -10,47 +10,75 @@
       class="
         block
         w-full
+        pr-8
         h-20
         resize-none
-        border-gray-300
         font-medium
         text-grey-900
+        border-gray-300
         placeholder-gray-400
-        focus:outline-none focus:ring-blue-500 focus:border-blue-500
-        caret-blue-600
         rounded-md
       "
-      :class="{ 'border-blue-300': focusStatus }"
+      :class="
+        errors.length
+          ? 'focus:border-transparent  focus:ring-red-600 border-red-600'
+          : 'focus:border-transparent focus:ring-blue-500'
+      "
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      ref="textarea"
       type="text"
       :placeholder="placeholder"
       :readonly="readonly"
     />
+    <div v-if="icon" class="absolute top-9 right-2">
+      <BaseIcon
+        v-if="!clickStatus"
+        @click="clickHandler($event)"
+        :name="icon"
+        class="h-5 w-5 cursor-pointer"
+        aria-hidden="true"
+      />
+      <BaseIcon v-else class="h-5 w-5 text-green-500" name="check-square" />
+    </div>
+    <ul
+      v-if="errors.length"
+      class="absolute left-0 -bottom-6 font-medium text-sm text-red-600"
+    >
+      <li v-for="(error, idx) in errors" :key="idx">{{ error }}</li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import useFocus from '@/composables/useFocus'
+import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'BaseTextarea',
   props: {
     modelValue: [String, Number],
     placeholder: String,
     label: String,
+    icon: String,
+    errors: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
     readonly: {
       type: Boolean,
       default: false
     }
   },
-  setup() {
-    const { focusStatus, focusChange } = useFocus()
-    return {
-      focusStatus,
-      focusChange
+  setup(_, { emit }) {
+    const clickStatus = ref(false)
+    function clickHandler(event) {
+      clickStatus.value = true
+      emit('iconEvent', event.target)
+      setTimeout(() => {
+        clickStatus.value = false
+      }, 1000)
     }
+    return { clickStatus, clickHandler }
   }
 })
 </script>
