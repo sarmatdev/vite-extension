75e95a6d752738e21c1a681e783cde366f08b0bc
sarmat <skhachirov12@gmail.com>
2021-11-30 20:08:50 +0300

    feat: add zip build

--- build/webpack/prod.ts
+++ build/webpack/prod.ts
@@ -1,6 +1,13 @@
 import merge from 'webpack-merge'
 import common from './common'
+const ZipPlugin = require('zip-webpack-plugin')
 
 export default merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new ZipPlugin({
+      path: '../',
+      filename: 'vite-extension.zip'
+    })
+  ]
 })
